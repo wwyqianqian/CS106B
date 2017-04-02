859010e93f1e27441e4ebd96abf4f1bd4c7120ed
wwyqianqian <1208331933@qq.com>
2017-04-03 01:02:27 +0800

    install XCode and run with Qt then debug

--- MetaAcademy/src/recursion.cpp
+++ MetaAcademy/src/recursion.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 
 //Milestone 1: Greatest Common Denominator
-// 第一题，最大公约数，辗转相除法。
+// 第一题，最大公约数，辗转相除法。
 //(24,7)->(7,3)->(3,1)->(1,0) b == 0 -> a = 1; endl
 int gcd(int a, int b) {
     if ( b == 0 ){
@@ -56,7 +56,7 @@ void serpinskii(GWindow &w, int leftX, int leftY, int size, int order) {
     else{
         serpinskii(w, leftX, leftY, size / 2.0, order - 1);  //以左上点为基点，相对父元素为 1/4 的小三角形。两条边重复。
         serpinskii(w, leftX + size / 2.0, leftY, size / 2.0, order - 1); //以上线中点为基点
-        serpinskii(w, leftX + size / 4.0, leftY + height/ 2.0, size / 2.0, order - 1);  //以左线中点为基点
+        serpinskii(w, leftX + size / 4.0, leftY + size * sqrt( 3 ) / 4.0, size / 2.0, order - 1);  //以左线中点为基点
     }
 }
 
@@ -71,14 +71,6 @@ void serpinskii(GWindow &w, int leftX, int leftY, int size, int order) {
 //image.setRGB(x, y, color);	          给像素上色
 //image.getWidth(); image.getHeight();  返回图形的宽高 单位px
 //image.inBounds(x, y);	                pixel (x, y)在界内，返回TRUE
-int floodFill(GBufferedImage& image, int x, int y, int newColor) {
-   oldColor = image.getRGB(x, y);
-   if(oldColor != newColor){
-        return fillColor(image, x, y, newColor, oldColor);
-   }
-   return 0;
-}
-
 int fillColor(GBufferedImage& image, int x, int y, int newColor, int oldColor) {
     int count;
 
@@ -93,6 +85,15 @@ int fillColor(GBufferedImage& image, int x, int y, int newColor, int oldColor) {
     return count;
 }
 
+int floodFill(GBufferedImage& image, int x, int y, int newColor) {
+   int oldColor = image.getRGB(x, y);
+   if(oldColor != newColor){
+        return fillColor(image, x, y, newColor, oldColor);
+   }
+   return 0;
+}
+
+
 
 
 
@@ -100,14 +101,6 @@ int fillColor(GBufferedImage& image, int x, int y, int newColor, int oldColor) {
 
 //Milestone 4: Personalized Curriculum
 //第四题，找出他所包含的私有课程，直到打印出自己personalCurriculum。
-void personalCurriculum(Map<string, Vector<string>> & prereqMap,string goal){
-
-    Set <string> alreadymentioned;
-    alreadymentioned.add(goal);
-    betterCurriculum(prereqMap, goal, alreadymentioned);
-    cout << goal << endl;
-
-}
 void betterCurriculum(Map<string, Vector<string>> & prereqMap, string goal, Set<string> & alreadymentioned){
 
     if (prereqMap.get(goal).isEmpty()){
@@ -124,6 +117,15 @@ void betterCurriculum(Map<string, Vector<string>> & prereqMap, string goal, Set<
     }
 }
 
+void personalCurriculum(Map<string, Vector<string>> & prereqMap,string goal){
+
+    Set <string> alreadymentioned;
+    alreadymentioned.add(goal);
+    betterCurriculum(prereqMap, goal, alreadymentioned);
+    cout << goal << endl;
+
+}
+
 
 
 
@@ -131,13 +133,6 @@ void betterCurriculum(Map<string, Vector<string>> & prereqMap, string goal, Set<
 
 //Milestone 5: Generate Question
 //第五题，组个句子？？？（（不是很懂
-
-string generate(Map<string, Vector<string> > & grammar, string symbol)
-{
-   string out = "";
-   return generateHelper ( grammar , symbol , out );
-}
-
 string generateHelper(Map<string, Vector<string> > & grammar, string symbol, string result){
     if(! grammar.containsKey (symbol)){
         return symbol;
@@ -152,4 +147,10 @@ string generateHelper(Map<string, Vector<string> > & grammar, string symbol, str
       }
     }
     return result;
+}
+
+string generate(Map<string, Vector<string> > & grammar, string symbol)
+{
+   string out = "";
+   return generateHelper ( grammar , symbol , out );
 }
